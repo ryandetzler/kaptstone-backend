e710bff21a6a369631e1e0530728abe916d98361
ryandetzler <detzlerryan@gmail.com>
2021-04-07 19:54:10 -0400

    Added middleware and starter code

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  next();
+});
+
 app.get('/', (req, res) => {
+  console.log("get request received")
   res.send('Hello World!')
 });
 
