33d82cbbdba4db1ba2e996706b026d419ea3f13f
ryandetzler <detzlerryan@gmail.com>
2021-04-13 15:01:30 -0400

    login, logout, createUser, get users, get user by id, post m message, and get message by id all converted to mongoose syntax and functional

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const { nanoid } = require('nanoid');
 const jwt = require('jsonwebtoken');
 const secret = "Group18";
 
 //mongodb starter code, copied and pasted from their site
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://detzlerryan@gmail.com:kapstone18@group-18-kapstone.kbjox.mongodb.net/db?retryWrites=true&w=majority";
+const uri = "mongodb+srv://rdetzler:kapstone18@group-18-kapstone.kbjox.mongodb.net/db?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
@@ -17,7 +16,8 @@ client.connect(err => {
 
 //mongoose start from their site
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
 app.use(express.json());
 
 //set up models for user and message using mongoose
@@ -46,10 +46,6 @@ const User = mongoose.model('User', {
     minLength: 1,
     maxLength: 200,
   },
-  id: {
-    type: String,
-    default: () => nanoid(),
-  },
   token: {
     type: String,
     default: "",
@@ -57,10 +53,6 @@ const User = mongoose.model('User', {
 });
 
 const Message = mongoose.model('Message', {
-    id: {
-      type: String,
-      default: () => nanoid()
-    },
     text: {
       type: String,
       maxLength: 200,
@@ -77,19 +69,19 @@ const Message = mongoose.model('Message', {
 });
 
 //middleware from Peter Mayor demo
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header(
-    "Access-Control-Allow-methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header(
+//     "Access-Control-Allow-methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
 
-  next();
-});
+//   next();
+// });
 
 app.use(express.json());
 
@@ -122,38 +114,35 @@ function checkAuth(req, res, next) {
   };
 };
 
-app.post('/auth/login', (req, res) => {
+app.post('/auth/login', async (req, res) => {
   const {username, password} = req.body;
-  const user = db.users.find((u) => {
-    return u.username === username
-  });
+  const user = await User.findOne({ username }).exec();
+
+  if (!user) {
+    res.status(400);
+    };
+
   if (user.password === password) {
-    const index = db.users.findIndex((u) => {
-      return u.id === user.id
-    });
     const token = jwt.sign({  foo: 'bar' }, secret);
-    db.users[index].token = token
+    const updatedUser = await User.updateOne({ username }, {token} );
 
-    res.send(token);
+    res.send(updatedUser);
   }
-  res.status(401);
+  res.status(401)
 });
 
-app.get('/auth/logout', (req, res) => {
+app.get('/auth/logout', checkAuth, async (req, res) => {
   const username = req.body;
-  const user = db.users.find((u) => {
-    return u.username === username
+  const user = await User.findOne({ username }).exec();
+  
+  const token = "";
+  const updatedUser = await User.updateOne({ username }, { token });
+
+  res.status(200).send(updatedUser)
   });
-  const index = db.users.findIndex((u) => {
-    return u.id === user.id
-  });
-  db.users[index].token = "";
-  res.send(token);
-});
 
 app.post('/users', (req, res) => {
-  const {username, displayName, password} = req.body;
-  const newUser = new User (req.body);
+  const user = new User(req.body);
   user.save().then(() => {
     res.json(user)
   }).catch((err) => {
@@ -170,11 +159,11 @@ app.get('/users', async (req, res) => {
   res.json(users)
 });
 
-app.get('/users/:username', (req, res) => {
-  const selectedUser = db.users.find((user) => {
-    return user.username === req.params.username
-  });
-  res.json(selectedUser)
+app.get('/users/:username', async (req, res) => {
+  const username = req.params.username;
+  const user = await User.findOne({ username }).exec();
+
+  res.json(user)
 });
 
 app.get('/users/:username/picture', (req, res) => {
@@ -186,17 +175,19 @@ app.put('/users/:username/picture', (req, res) => {
 });
 
 app.post('/messages', (req, res) => {
-  const message = {
-    text: req.body.text,
-    id: nanoid(),
-  };
-  db.messages.push(message);
-  res.status(201).json(message);
+  const message = new Message(req.body)
+  message.save().then(() => {
+    res.json(message)
+  }).catch((err) => {
+    res.status(400).send(err)
+  });
 });
 
-app.get('/messages/:messageId', (req, res) => {
-  const selectedMessage = db.messages.find((message) => message.id == req.params.messageId);
-    res.json(selectedMessage);
+app.get('/messages/:messageId', async (req, res) => {
+  const messageId = req.params.messageId;
+  const message = await Message.findById(messageId).exec();
+
+  res.json(message);
 });
 
 app.get('/messages}', (req, res) => {
